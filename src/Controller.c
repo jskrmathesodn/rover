cd3d19ec440a294c96111f2821013792b2b33b11
jskrmathesodn <jmatheson749@gmail.com>
2026-05-02 11:00:09 +0800

    controlled driving with battery monitor

--- src/Controller.c
+++ src/Controller.c
@@ -9,31 +9,38 @@ serial2_init();
 char reading1[24], reading2[24];
 uint32_t joystick1_value, joystick2_value;
 uint32_t current_ms, last_send_ms;
+uint8_t recievedData;
+uint32_t battery_percentage;
 serial0_init();
 milliseconds_init();
 adc_init();
+lcd_init();
+DDRD = 0x00; //put PORTD into input mode
+PORTD |= (1<<PD0); //enable the internal pull-up resistor on PD0
 
 while(1)
 {
 	//main loop
   if(serial2_available()) //Returns true if new data available on serial buffer
 	{
+    serial2_get_data(recievedData,1); //Function takes the array to return data to and the number of bytes to be read.
 	}
   current_ms = milliseconds_now();
-	
-	//sending section
-	if( (current_ms-last_send_ms) >= 100) //sending rate controlled here
+
+  battery_percentage = recievedData*100/118;
+  lcd_home();
+  lcd_puts(battery_percentage);
+  sprintf(reading1,"Battery: %3u%% \n", battery_percentage); //Format string
+  serial0_print_string(reading1); //Print string to terminal
+
+	if ((current_ms-last_send_ms) >= 100) //sending rate controlled here
 	{
-  joystick1_value = adc_read(0)/4;
-  joystick2_value = adc_read(1)/4;
-  sprintf(reading1, "Joystick 1 Value: %lu", joystick1_value);
-  sprintf(reading2, "Joystick 2 Value: %lu", joystick2_value);
-  serial0_print_string(reading1);
-  serial0_print_string("\n");
-  serial0_print_string(reading2);
-  serial0_print_string("\n");
-  serial2_write_bytes(2, reading1, reading2);
-  _delay_ms(1000);
+  joystick1_value = abs((adc_read(15)/5)-204);
+  joystick2_value = abs((adc_read(0)/5)-204);
+  if (joystick1_value > 255) joystick1_value = 255;
+  if (joystick2_value > 255) joystick2_value = 255;
+  uint8_t data_to_send[2] = {joystick1_value, joystick2_value};
+  serial2_write_bytes(2, data_to_send[0], data_to_send[1]);
   last_send_ms = current_ms;
   }
 }
--- src/Robot.c
+++ src/Robot.c
@@ -10,43 +10,86 @@ int main(void)
 {
 //main function initialization
 serial2_init();
-char reading[10];
-uint32_t adc_value;
-uint32_t volt_value;
-uint32_t range;
 serial0_init();
 milliseconds_init();
+adc_init();
+uint32_t current_ms, last_send_ms;
+uint8_t battery_voltage;
+static int16_t lm = 0;
+static int16_t rm = 0;
+static int16_t fc;
+static int16_t rc;
 char buffer[60];
 char serial_string[60];
-uint8_t command;
 uint8_t recievedData[2]; //recieved data array
-uint32_t current_reading;
-uint32_t current_ms, last_send_ms;
 uint16_t joystick;
 DDRF = 0x00;//put PORTF into input mode
 PORTF |= (1<<PF0); //enable the internal pull-up resistor on PF0 and leave the rest of PORTF alone
-DDRB = 0xFF;
+DDRB |= (1<<PB5) | (1<<PB6); // OC1A and OC1B as outputs
+DDRA |= (1<<PA0)|(1<<PA1)|(1<<PA2)|(1<<PA3); //put A0-A3 into low impedance output mode
 // Confuguring Timer1
 TCCR1A = 0; TCCR1B = 0;
 TCCR1A |= (1 << WGM11);
-TCCR1B |= (1 << WGM13); // Mode 10, Phase correct
-TCCR1A |= (1 << COM1A1)|(1<<COM1B1); // Clear down, set on upcount
+TCCR1B |= (1 << WGM13) | (1 << WGM12); // Fast PWM, TOP = ICR1
+TCCR1A |= (1 << COM1A1) | (1 << COM1B1);
 TCCR1B |= (1 << CS11); // Prescaler = 8
 
-ICR1 = 20000; // Set TOP value for 20ms period (50Hz)
+ICR1 = 10000; // Set TOP value for 20ms period (50Hz)
 
 while(1)
 {
 	//main loop
-  if(serial2_available()) //Returns true if new data available on serial buffer
+    if(serial2_available()) //Returns true if new data available on serial buffer
 	{
-		//Function takes the array to return data to and the number of bytes to be read.
-		serial2_get_data(recievedData,2); 
-    sprintf(serial_string,"Joystick 1: %3u Joystick 2: %3u", recievedData[0], recievedData[1]); //Format string
+        //Function takes the array to return data to and the number of bytes to be read.
+        serial2_get_data(recievedData,2); 
+        fc = ((int16_t)recievedData[0]*5)/4;
+        rc = ((int16_t)recievedData[1]*5)/4;
+        sprintf(serial_string,"Joystick 1: %3u Joystick 2: %3u \n", fc, rc); //Format string
+    }
+
+    lm = fc - 127;
+    rm = rc - 127;
+    if (lm > 127) lm = 127;
+    if (lm < -127) lm = -127;
+    if (rm > 127) rm = 127;
+    if (rm < -127) rm = -127;
+    sprintf(serial_string,"Joystick 1: %3d Joystick 2: %3d \n", lm, rm); //Format string
     serial0_print_string(serial_string); //Print string to terminal
-    OCR1A = 1000 + ((4 * (uint32_t)recievedData[0]) * 1000) / 1023;
-    OCR1B = 1000 + ((4 * (uint32_t)recievedData[1]) * 1000) / 1023;
-	}
+    OCR1A = (int32_t)abs(lm)*10000/127;
+    OCR1B = (int32_t)abs(rm)*10000/127;
+    if(lm>=0)
+    {
+        //set direction of left motor forward
+        PORTA |= (1<<PA0);
+        PORTA &= ~(1<<PA1);
+    }
+    else
+    {
+        //set direction of left motor in reverse
+        PORTA |= (1<<PA1);
+        PORTA &= ~(1<<PA0);
+    }
+    if(rm>=0)
+    {
+        //set direction of right motor forward
+        PORTA |= (1<<PA2);
+        PORTA &= ~(1<<PA3);
+    }
+    else
+    {
+        //set direction of right motor in reverse
+        PORTA |= (1<<PA3);
+        PORTA &= ~(1<<PA2);
+    }
+    current_ms = milliseconds_now();
+	
+	//sending section
+	if( (current_ms-last_send_ms) >= 100) //sending rate controlled here
+	{
+        battery_voltage = adc_read(7)/5; //read battery voltage on ADC channel 7, convert to voltage
+        serial2_write_bytes(1, battery_voltage);
+        last_send_ms = current_ms;
   }
 }
 }
